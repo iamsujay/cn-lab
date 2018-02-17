e39923e51270bec898c7f66e11d3e716fbb4064b
Sujay Hegde <sujay@localhost.localdomain>
2018-02-17 13:13:40 +0530

    Code refactored, tested

--- bitStuffing.c
+++ bitStuffing.c
@@ -1,59 +1,61 @@
+/* dataframe is a string of bits with FLAG at both ends
+ * sender()   - add a 0 in msg after five 1's in data
+ * reciever() - remove 0 for data after five 1's in msg
+ */ 
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #define FLAG "01111110"
+#define SIZE 200
 
-void sender();
+void sender(char* data);
 void reciever(char* msg);
 
 void main()
 {
-	sender();
+	char data[SIZE];
+	printf("Enter the frame bits : ");
+	scanf("%s", data);
+	sender(data);
 }
 
-void sender()
+void sender(char* data)
 {
-	printf("Enter the frame bits : ");
-	char data[200], msg[256];
-	scanf("%s", data);
-
-	strcpy(msg, FLAG);
-	int count = 0, i, j;
-	for( i = strlen(msg), j = 0; data[j] != '\0'; i++, j++)
+	char msg[SIZE + 40];
+	strcpy(msg, FLAG);				// message to start with FLAG
+	int oneCount = 0, i, j;			
+	for(i=0, j=strlen(msg); data[i]!= '\0'; msg[j++] = data[i++])
 	{
-		msg[i] = data[j];
-		if(msg[i] == '1')
-			count++;
-		if(count == 5)
+		if(oneCount == 5)
 		{
-			msg[++i] = '0';
-			count = 0;
+			msg[j++] = '0';					
+			oneCount = 0;
 		}
+		if(data[i] == '1')
+			oneCount++;
 	}
-	msg[i] = '\0';
-	strcat(msg, FLAG );
+	strcpy(msg+j, FLAG );			// message to end with FLAG
 	printf("Sent msg : %s \n", msg);
 	reciever(msg);
 }
 
 void reciever(char* msg)
+{
+	msg = strstr(msg, FLAG) + strlen(FLAG); 	// fwd begining FLAG
+	*(strstr(msg, FLAG)) = '\0'; 				// trim ending FLAG
+
+	char data[SIZE];
+	int oneCount = 0, i, j;
+	for(i=0, j=0; msg[i] != '\0'; data[j++] = msg[i++] )
 	{
-	char data[256], bit;
-	int count = 0, i, j;
-	
-	msg = strstr(msg, FLAG) + strlen(FLAG);
-	*(strstr(msg, FLAG)) = '\0';
-	
-	for( i = 0, j = 0; msg[i] != '\0'; i++, j++)
-	{
-		data[j] = msg[i];
-		if(msg[i] == '1')
-				count++;
-		if(count == 5 && ( msg[i+1] == '0'))
+		if((oneCount == 5) && (msg[i] == '0'))
 		{
-			i++;
-			count = 0;
+			++i;
+			oneCount = 0;
 		}
+		if(msg[i] == '1')
+				oneCount++;
 	}
 	data[j] = '\0';
 	printf("Decoded data : %s \n", data);
