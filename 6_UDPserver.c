9525e616f1b1718ec39b0126e744a38952358cad
Sujay N Hegde <sujay9626@gmail.com>
2018-02-19 21:38:26 +0530

    Update 6_UDPserver.c

--- 6_UDPserver.c
+++ 6_UDPserver.c
@@ -1,4 +1,4 @@
- #include <sys/socket.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -11,13 +11,13 @@ void main()
 	int servSock = socket(AF_INET,SOCK_DGRAM,0);
 
 	struct sockaddr_in servAddr;
-	servAddr.sin_family		 = AF_INET;
+	servAddr.sin_family 	 = AF_INET;
 	servAddr.sin_addr.s_addr = INADDR_ANY;
 	servAddr.sin_port 		 = htons(PORT);
 
 	bind(servSock, (struct sockaddr*) &servAddr, sizeof(servAddr));
 	
 	char buf[1024];
-	while( recvfrom(servSock, &buf, sizeof(buf), 0, NULL, NULL) )
-	    write(1, buf, strlen(buf));
+	recvfrom(servSock, &buf, sizeof(buf), 0, NULL, NULL);
+	write(1, buf, strlen(buf));
 }
